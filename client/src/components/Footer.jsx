df56fcacbda08eb1d3313566a4ed9795b2358791
Semicolon <adedamola13@gmail.com>
2025-07-13 09:50:41 +0100

    adding edits to the footer section

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -74,8 +74,8 @@ const Footer = () => {
                 <div>
                     <h2 className='text-base font-medium text-gray-800 uppercase'>Contact</h2>
                     <ul className='mt-3 flex flex-col gap-1.5'>
-                        <li>1234 Luxury Drive</li>
-                        <li>San Francisco, CA 94107</li>
+                        <li>001, herbert macualay way</li>
+                        <li>Yaba, Lagos</li>
                         <li>+234 123 456789</li>
                         <li>info@example.com</li>
                     </ul>
